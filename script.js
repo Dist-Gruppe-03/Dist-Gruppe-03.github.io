ee6821f70cf5b6ab62d32c7b539b26dbcc3a74ec
KredeDK <krede92@gmail.com>
2018-03-20 02:48:39 +0100

    bum bum

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-			$( document ).ready(function() {
-				$("#login_form").submit(function(event){
-					event.preventDefault();
-					console.log($(this).serializeArray());
-					<!--$.post("api", $(this).serializeArray(), function(result){ -->
-					$.get("https://dawa.aws.dk/autocomplete?kommunekode=101&q=" + $("#letter").val() + "&type=adresse&caretpos=2&supplerendebynavn=true&stormodtagerpostnumre=true&multilinje=true&fuzzy=", function(result){
-					
-						console.log(result);
-						$("body").removeClass("not-logged-in").addClass("logged-in");
-					});
-				});
-				
-				$("#guess_form").submit(function(event){
-					event.preventDefault();
-					console.log($(this).serializeArray());
-					$.get("https://dawa.aws.dk/autocomplete?kommunekode=101&q=" + $("#letter").val() + "&type=adresse&caretpos=2&supplerendebynavn=true&stormodtagerpostnumre=true&multilinje=true&fuzzy=", function(result){
-						console.log(result);
-						$("#result").text(result[0]['tekst']);
-					});
-				});
-			});
+$( document ).ready(function() {
+	$("#login_form").submit(function(event){
+		event.preventDefault();
+		console.log($(this).serializeArray());
+		<!--$.post("api", $(this).serializeArray(), function(result){ -->
+		$.get("https://dawa.aws.dk/autocomplete?kommunekode=101&q=" + $("#letter").val() + "&type=adresse&caretpos=2&supplerendebynavn=true&stormodtagerpostnumre=true&multilinje=true&fuzzy=", function(result){
+		
+			console.log(result);
+			$("body").removeClass("not-logged-in").addClass("logged-in");
+		});
+	});
+	
+	$("#guess_form").submit(function(event){
+		event.preventDefault();
+		console.log($(this).serializeArray());
+		$.get("http://localhost:8080/RestService/webresources/generic/jsontest", function(result){
+		
+		//$.get("https://dawa.aws.dk/autocomplete?kommunekode=101&q=" + $("#letter").val() + "&type=adresse&caretpos=2&supplerendebynavn=true&stormodtagerpostnumre=true&multilinje=true&fuzzy=", function(result){
+			console.log(result);
+			//$("#result").text(result[0]['kredit']);
+			$("#result").text(result['json']);
+		});
+	});
+});
