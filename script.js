31d604dd403663ac16dc7245f8a3ef431e7e8bd5
Christian <krede92@gmail.com>
2018-03-22 08:14:47 +0100

    work in progress

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-$( document ).ready(function() {
+$(document).ready(function() {
+	
 	$("#login_form").submit(function(event){
 		event.preventDefault();
 		console.log($(this).serializeArray());
@@ -8,17 +9,20 @@ $( document ).ready(function() {
 			console.log(result);
 			$("body").removeClass("not-logged-in").addClass("logged-in");
 		});
+		$("#login_form").hide();
 	});
+	
+	
 	
 	$("#guess_form").submit(function(event){
 		event.preventDefault();
-		console.log($(this).serializeArray());
-		$.get("http://localhost:8080/RestService/webresources/generic/jsontest", function(result){
+		$.get("http://localhost:8080/RestService/webresources/generic/jsontest?letter=" + $("#letter").val(), function(result){
 		
 		//$.get("https://dawa.aws.dk/autocomplete?kommunekode=101&q=" + $("#letter").val() + "&type=adresse&caretpos=2&supplerendebynavn=true&stormodtagerpostnumre=true&multilinje=true&fuzzy=", function(result){
 			console.log(result);
-			//$("#result").text(result[0]['kredit']);
-			$("#result").text(result['json']);
-		});
+			document.getElementById("result").innerHTML = "Who: " + result.item1;
+			document.getElementById("sendguess").innerHTML = "Du gættede på: " + $("#letter").val().toUpperCase();
+			document.getElementById("invisibleword").innerHTML = "Who: " + result.item3;
+			});
 	});
 });
