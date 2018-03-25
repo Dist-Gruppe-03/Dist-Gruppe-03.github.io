d0dd5f4134c676ab6cfd4456cff0cfc4668942b1
KredeDK <krede92@gmail.com>
2018-03-26 01:03:24 +0200

    Todays changes

--- script.js
+++ script.js
@@ -16,13 +16,13 @@ $(document).ready(function() {
 	
 	$("#guess_form").submit(function(event){
 		event.preventDefault();
-		$.get("http://localhost:8080/RestService/webresources/generic/jsontest?letter=" + $("#letter").val(), function(result){
+		$.get("http://localhost:8080/RestService/hangman/play/jsontest?letter=" + $("#letter").val(), function(result){
 		
 		//$.get("https://dawa.aws.dk/autocomplete?kommunekode=101&q=" + $("#letter").val() + "&type=adresse&caretpos=2&supplerendebynavn=true&stormodtagerpostnumre=true&multilinje=true&fuzzy=", function(result){
 			console.log(result);
 			document.getElementById("result").innerHTML = "Who: " + result.item1;
 			document.getElementById("sendguess").innerHTML = "Du gættede på: " + $("#letter").val().toUpperCase();
-			document.getElementById("invisibleword").innerHTML = "Who: " + result.item3;
+			document.getElementById("invisibleword").innerHTML = "Ord: " + result.item2;
 			});
 	});
 });
