ab5588886ba1a910f4a3c556cd7046ef47e809ed
KredeDK <krede92@gmail.com>
2018-04-21 05:32:12 +0200

    Game works now
    Needs some fine adjustment, login not implemented yet

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	$("#login_form").submit(function(event){
 		event.preventDefault();
 		console.log($(this).serializeArray());
-		<!--$.post("api", $(this).serializeArray(), function(result){ -->
+		//<!--$.post("api", $(this).serializeArray(), function(result){ -->
 		$.get("https://dawa.aws.dk/autocomplete?kommunekode=101&q=" + $("#letter").val() + "&type=adresse&caretpos=2&supplerendebynavn=true&stormodtagerpostnumre=true&multilinje=true&fuzzy=", function(result){
 		
 			console.log(result);
@@ -12,17 +12,28 @@ $(document).ready(function() {
 		$("#login_form").hide();
 	});
 	
-	
+		$.get("http://localhost:8080/RestServer/hangman/play/json/s114992", function(result){
+		document.getElementById("usedletters").innerHTML = result.usedletters;
+		document.getElementById("invisibleword").innerHTML = result.invisibleword;
+	});
 	
 	$("#guess_form").submit(function(event){
 		event.preventDefault();
-		$.get("http://localhost:8080/RestService/hangman/play/jsontest?letter=" + $("#letter").val(), function(result){
+		$.get("http://localhost:8080/RestServer/hangman/play/json/s114992?letter=" + $("#letter").val(), function(result){
 		
-		//$.get("https://dawa.aws.dk/autocomplete?kommunekode=101&q=" + $("#letter").val() + "&type=adresse&caretpos=2&supplerendebynavn=true&stormodtagerpostnumre=true&multilinje=true&fuzzy=", function(result){
+			$("#image").attr("src","grafik/forkert" + result.wrongletters + ".png");
 			console.log(result);
-			document.getElementById("result").innerHTML = "Who: " + result.item1;
-			document.getElementById("sendguess").innerHTML = "Du gættede på: " + $("#letter").val().toUpperCase();
-			document.getElementById("invisibleword").innerHTML = "Ord: " + result.item2;
+			document.getElementById("result").innerHTML = result.response;
+			document.getElementById("usedletters").innerHTML = result.usedletters;
+			document.getElementById("invisibleword").innerHTML = result.invisibleword;
+			$("#letter").val("");
 			});
 	});
 });
+							/* 
+							ids:
+							feedback = login failed/succeed
+							sendguess = der gættes på
+							invisibleword = usynligt ord som skal gættes
+							wrongguesses = forkerte ord
+							*/
