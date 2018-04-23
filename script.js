fc5abb9c725b118316a9a786311f335e88cd4964
KredeDK <krede92@gmail.com>
2018-04-24 00:13:24 +0200

    1

--- script.js
+++ script.js
@@ -12,14 +12,14 @@ $(document).ready(function() {
 		$("#login_form").hide();
 	});
 	
-		$.get("http://ubuntu4.saluton.dk:38055/RestServer/hangman/play/json/s114992?reset=true", function(result){
+		$.get("https://ubuntu4.saluton.dk:38055/RestServer/hangman/play/json/s114992?reset=true", function(result){
 		document.getElementById("usedletters").innerHTML = result.usedletters;
 		document.getElementById("invisibleword").innerHTML = result.invisibleword;
 	});
 	
 	$("#guess_form").submit(function(event){
 		event.preventDefault();
-		$.get("http://ubuntu4.saluton.dk:38055/RestServer/hangman/play/json/s114992?letter=" + $("#letter").val(), function(result){
+		$.get("https://ubuntu4.saluton.dk:38055/RestServer/hangman/play/json/s114992?letter=" + $("#letter").val(), function(result){
 		
 			$("#image").attr("src","grafik/forkert" + result.wrongletters + ".png");
 			document.getElementById("result").innerHTML = result.response;
@@ -36,7 +36,7 @@ $(document).ready(function() {
 	});
 	
 	$( "#button" ).click(function() {
-		$.get("http://ubuntu4.saluton.dk:38055/RestServer/hangman/play/json/s114992?reset=true", function(result){
+		$.get("https://ubuntu4.saluton.dk:38055/RestServer/hangman/play/json/s114992?reset=true", function(result){
 			$("#retry_button").hide();
 			$("#guess_box").show();
 			document.getElementById("result").innerHTML = result.response;
