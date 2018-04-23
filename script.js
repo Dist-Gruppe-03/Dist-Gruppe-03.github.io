2bcc3327fba2e07d0e8da7712aaee990df26d6f9
KredeDK <krede92@gmail.com>
2018-04-23 23:51:50 +0200

    Works with online server
    http://ubuntu4.saluton.dk:38055/RestServer/

--- script.js
+++ script.js
@@ -12,28 +12,37 @@ $(document).ready(function() {
 		$("#login_form").hide();
 	});
 	
-		$.get("http://localhost:8080/RestServer/hangman/play/json/s114992", function(result){
+		$.get("http://ubuntu4.saluton.dk:38055/RestServer/hangman/play/json/s114992?reset=true", function(result){
 		document.getElementById("usedletters").innerHTML = result.usedletters;
 		document.getElementById("invisibleword").innerHTML = result.invisibleword;
 	});
 	
 	$("#guess_form").submit(function(event){
 		event.preventDefault();
-		$.get("http://localhost:8080/RestServer/hangman/play/json/s114992?letter=" + $("#letter").val(), function(result){
+		$.get("http://ubuntu4.saluton.dk:38055/RestServer/hangman/play/json/s114992?letter=" + $("#letter").val(), function(result){
 		
 			$("#image").attr("src","grafik/forkert" + result.wrongletters + ".png");
-			console.log(result);
 			document.getElementById("result").innerHTML = result.response;
 			document.getElementById("usedletters").innerHTML = result.usedletters;
 			document.getElementById("invisibleword").innerHTML = result.invisibleword;
-			$("#letter").val("");
+			$("#letter").val(""); // empty box
+			
+			if(result.gameover === "true") {
+				$("#guess_box").hide();
+				$("#retry_button").show();
+				document.getElementById("result2").innerHTML = result.response;
+			} 
 			});
 	});
+	
+	$( "#button" ).click(function() {
+		$.get("http://ubuntu4.saluton.dk:38055/RestServer/hangman/play/json/s114992?reset=true", function(result){
+			$("#retry_button").hide();
+			$("#guess_box").show();
+			document.getElementById("result").innerHTML = result.response;
+			document.getElementById("usedletters").innerHTML = result.usedletters;
+			document.getElementById("invisibleword").innerHTML = result.invisibleword;
+		});
+	});
+	
 });
-							/* 
-							ids:
-							feedback = login failed/succeed
-							sendguess = der gættes på
-							invisibleword = usynligt ord som skal gættes
-							wrongguesses = forkerte ord
-							*/
